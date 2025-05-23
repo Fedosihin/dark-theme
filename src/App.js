6a437055bc473497aafc14eddbd9bf4922bc518e
German <fedosihingerman@yandex.ru>
2025-05-23 05:10:55 +0300

    time added and css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [darkTheme, setTheme] = useState(false);
+
+  useEffect(() => {
+    const myElement = document.querySelector("body");
+    if (darkTheme){
+      myElement.classList.remove("dark-theme");
+    } else {
+      myElement.classList.add("dark-theme");
+    }
+  }, [darkTheme]);
+
+  const now = new Date();
+  const timeString = now.toLocaleTimeString(); // Например: "14:25:07"
+
   return (
-    <div className="App">
-      <p>State: {darkTheme.toString()}</p>
-      <button onClick={()=>setTheme(init => !init)}>button</button>
+    <div
+      className="App"
+      // style={{
+      //   color: "black",
+      //   backgroundColor: darkTheme ? "black" : "white",
+      //   padding: "10px",
+      //   minHeight: "100vh",
+      // }}
+    >
+      <p>Current time: {timeString}</p>
+      <p
+        // style={{
+        //   color: darkTheme ? "white" : "black",
+        // }}
+      >
+        theme: {darkTheme ? "dark" : "white"}
+      </p>
+      <button onClick={() => setTheme((init) => !init)}>Change theme</button>
     </div>
   );
 }
