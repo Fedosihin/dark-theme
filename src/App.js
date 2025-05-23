a898c9fe9bf6839c26acf64cb8b93fb5dc4091f4
German <fedosihingerman@yandex.ru>
2025-05-23 04:35:31 +0300

    simple state + button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [darkTheme, setTheme] = useState(false);
   return (
     <div className="App">
+      <p>State: {darkTheme.toString()}</p>
+      <button onClick={()=>setTheme(init => !init)}>button</button>
     </div>
   );
 }
